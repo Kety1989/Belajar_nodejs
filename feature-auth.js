7f34c1b24e56a1c5cbccdb0c8cceb3ba68e3f3d8
Kety Kusmawati <Y136@BANKBJB.CO.ID>
2022-10-05 16:10:25 +0700

    init file

---
+++ feature-auth.js
@@ -0,0 +1,9 @@
+const Login = () =>{
+    console.log('Login');
+}
+
+const Register = () => {
+    console.log('Register')
+}
+
+module.exports = { Login, Register}
---
+++ index.js
@@ -0,0 +1,9 @@
+const os = require("os");
+const {Login, Register} = require("./feature-auth");
+
+Login();
+Register();
+
+// console.log(os.freemeem());
+// console.log(`hello word`);
+// console.log(`hello Lagi, skjdhksd`);
